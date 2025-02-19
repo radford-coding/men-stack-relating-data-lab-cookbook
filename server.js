e14aafc148292cf10a9ceb3f5a8f7ee6c23d9a85
Matt Radford <quinquex@gmail.com>
2025-02-19 14:17:51 -0800

    custom middleware

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
 const recipesController = require('./controllers/recipes.js');
 const ingredientsController = require('./controllers/ingredients.js');
@@ -29,7 +32,10 @@ app.use(
     saveUninitialized: true,
   })
 );
+
+app.use(passUserToView);
 app.use('/auth', authController);
+app.use(isSignedIn);
 app.use('/recipes', recipesController);
 app.use('/ingredients', ingredientsController);
 
